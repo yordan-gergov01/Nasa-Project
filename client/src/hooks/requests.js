70b031ff1632796af02a99303d78ef0646498a1c
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-24 15:05:10 +0200

    implement cors middleware

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,6 +1,10 @@
+const API_URL = "http://localhost:8000";
+
+// Load planets and return as JSON.
 async function httpGetPlanets() {
-  // TODO: Once API is ready.
-  // Load planets and return as JSON.
+  const response = await fetch(`${API_URL}/planets`);
+  const planets = await response.json();
+  return planets;
 }
 
 async function httpGetLaunches() {
@@ -18,9 +22,4 @@ async function httpAbortLaunch(id) {
   // Delete launch with given ID.
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
