679c979f7d9b440edc0d24009c4fdee8fbd0774c
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-25 11:00:09 +0200

    validation for post request

--- server/src/routes/launches/launchesController.js
+++ server/src/routes/launches/launchesController.js
@@ -7,7 +7,23 @@ function httpGetAllLaunches(req, res) {
 function httpCreateNewLaunch(req, res) {
   const launch = req.body;
 
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property.",
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
 
   return res.status(201).json(addNewLaunch(launch));
 }
