ef96721dabf55901c50435ff0aa56911210c5f37
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-28 10:59:41 +0200

    creating and inserting planets into database

--- server/src/models/planetsModel.js
+++ server/src/models/planetsModel.js
@@ -4,8 +4,6 @@ const { parse } = require("csv-parse");
 
 const planets = require("./planetsMongo");
 
-const habitablePlanets = [];
-
 function isHabitablePlanet(planet) {
   return (
     planet["koi_disposition"] === "CONFIRMED" &&
@@ -28,17 +26,16 @@ function loadPlanetsData() {
       )
       .on("data", async (data) => {
         if (isHabitablePlanet(data)) {
-          // await planets.create({
-          //   keplerName: data.kepler_name,
-          // });
+          savePlanets(data);
         }
       })
       .on("error", (err) => {
         console.log(err);
         reject(err);
       })
-      .on("end", () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
+      .on("end", async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} habitable planets found!`);
         resolve();
       });
   });
@@ -46,6 +43,25 @@ function loadPlanetsData() {
 
 async function getAllPlanets() {
   return await planets.find({});
+}
+
+async function savePlanets(planet) {
+  try {
+    await planets.updateOne(
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        // When true, creates a new document if no document matches the query
+        upsert: true,
+      }
+    );
+  } catch (err) {
+    console.error(`Could not save planet: ${err}`);
+  }
 }
 
 module.exports = {
