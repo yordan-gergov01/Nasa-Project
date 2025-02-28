ce3dc6541c70288339b793a5b64658de20de3c7d
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-28 12:54:49 +0200

    implement referential integrity

--- server/src/models/launchesModel.js
+++ server/src/models/launchesModel.js
@@ -1,4 +1,5 @@
 const launches = require("./launchesMongo");
+const planets = require("./planetsMongo");
 
 let latestFlightNumber = 100;
 
@@ -24,6 +25,14 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error("No matching planet found");
+  }
+
   await launches.updateOne(
     {
       flightNumber: launch.flightNumber,
