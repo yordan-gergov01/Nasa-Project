86f527094cf3ba1bc2cc19026e0f112076fdeff8
yordan-gergov01 <yordansoftskills@gmail.com>
2025-02-28 17:22:26 +0200

    versioning api

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const planetsRouter = require("./routes/planets/planetsRouter");
-const launchesRouter = require("./routes/launches/launchesRouter");
+const api = require("./routes/api");
 
 const app = express();
 
@@ -17,8 +16,8 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use("/planets", planetsRouter);
-app.use("/launches", launchesRouter);
+app.use("/v1", api);
+
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
