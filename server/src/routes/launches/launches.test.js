1c02f4bef948f06633e4167d85b98fdfa9e036b1
yordan-gergov01 <yordansoftskills@gmail.com>
2025-03-07 15:28:36 +0200

    writing quick test

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test GET /launches", () => {
+  test("It should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
